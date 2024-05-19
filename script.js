774782ec7ca08982d88b03a80d71a1b6a0ec1c76
chohanac <anaschohanto@gmail.com>
2024-05-19 13:22:20 -0400

    completed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function getComputerChoice()
 { 
-    let number = Math.random()*3;
-    if (number<1)
+    let number = Math.random()*3;   //multiple random number by 3. The range the random number lands corresponds to the rock, paper, scissors selection of computer
+    if (number<1)       
     {
-        return "rock";
+        return "r";
     }
     else if (number<2)
     {
-        return "paper";
+        return "p";
     }
     else
     {
-        return "scissors";
+        return "s";
     }
 }
 
@@ -19,15 +19,26 @@ function getHumanChoice()
 {
     let choice;
     let valid = false;
-    while (!valid)
+    while (!valid)           //loops until user selects a valid option
     {
         choice = prompt("Select rock, paper or scissors:")
         choice = choice.toLowerCase();
-        if (choice=="rock" || choice == "paper" || choice=="scissors")
+        switch (choice)     // if user types rock, paper, or scissors then there selection is change to r, p, or s. 
+        {
+            case "rock":
+                choice = "r";
+                break;
+            case "paper":
+                choice = "p";
+                break;
+            case "scissors":
+                choice = "s";        
+        }
+        if (choice=="r" || choice == "p" || choice=="s")  //checks if choice is valid
         {
             valid = true;
         }
-        else
+        else    // asks the user to select another option
         {
             console.log("Please select a valid option.");
         }
@@ -36,5 +47,90 @@ function getHumanChoice()
 
 }
 
-console.log(getComputerChoice());
-getHumanChoice();
+function playRound()
+{
+    let computer = getComputerChoice();
+    let user = getHumanChoice();
+    if (user == "r")
+    {
+        if (computer == "p")
+        {
+            console.log("You lost. Paper beats rock.");
+            computerScore++;
+        }
+        else if (computer == "s")
+        {
+            console.log("You win. Rock beats scissors");
+            humanScore++;
+        }
+        else 
+        {
+            console.log("Tie. You both selected rock.");
+        }
+    }
+    else if (user == "p")
+    {
+        if (computer == "r")
+            {
+                console.log("You win. Paper beats rock.");
+                humanScore++;
+            }
+        else if (computer == "s")
+            {
+                console.log("You lost. Scissors beats paper");
+                computerScore++;
+            }
+        else 
+            {
+                console.log("Tie. You both selected paper.");
+            }
+    }
+    else
+    {
+        if (computer == "p")
+        {
+            console.log("You win. Scissors beats paper.");
+            humanScore++;
+        }
+        else if (computer == "r")
+        {
+            console.log("You lost. rock beats scissors");
+            computerScore++;
+        }
+        else 
+        {
+            console.log("Tie. You both selected scissors.");
+        }
+    }
+}
+
+function playGame()
+{
+    let rounds = 0;
+    while (rounds<5)
+    {
+        playRound();
+        rounds++;
+        if (humanScore > ((5-rounds)+computerScore) || computerScore > ((5-rounds)+humanScore))   //if the computer's current score is greater than the maximum score the user could get with the remain rounds left (or vice versa) then the game ends
+        {
+            break;
+        }
+    }
+    if (humanScore > computerScore)
+    {
+            console.log(`You win! ${humanScore}-${computerScore}`);
+    }
+    else if (computerScore > humanScore)
+    {
+            console.log(`You lost! ${computerScore}-${humanScore}`);
+    }
+    else
+    {
+        console.log(`Tie game ${humanScore}-${computerScore}`);
+    }
+}
+
+humanScore = 0;
+computerScore = 0;
+console.log("Let's play rock, paper, scissors! 5 rounds")
+playGame();
